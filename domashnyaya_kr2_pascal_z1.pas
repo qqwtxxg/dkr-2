9b7bc1b347f689aee331ef273cd2149beabb1083
qqwtxxg <144107505+qqwtxxg@users.noreply.github.com>
2023-12-07 14:59:04 +0300

    Add files via upload

---
+++ domashnyaya_kr2_pascal_z1.pas
@@ -0,0 +1,38 @@
+﻿program mass_1dkr2;
+var
+  arr:array of integer;
+  dl,c,i,min,max,sum:integer;
+  begin
+    writeln('Введите длину массива: ');
+    readln(dl);
+    SetLength(arr,dl);
+    
+    write('Введите элементы массива: ');
+    for i:=0 to dl-1 do 
+    begin
+      readln(c);
+      arr[i]:=c;
+    end;
+    
+    {Находим минимальный элемент}
+    min := arr[1];
+    for i := 0 to dl-1 do
+    begin
+      if min > arr[i] then 
+        min := arr[i];
+    end;
+    writeln(arr);
+    writeln('Минимальный элемент массива: ', min); 
+    
+    {Находим максимальный элемент}
+    max:=arr[1];
+    for i := 0 to dl-1 do
+    begin
+      if max < arr[i] then 
+        max := arr[i];
+    end;
+    writeln('Максимальный элемент массива: ', max);
+    
+    sum := max+min;
+    writeln('Сумма максимального и минимального элементов массива: ', sum); 
+  end.
---
+++ domashnyaya_kr2_pascal_z2 (1).pas
@@ -0,0 +1,31 @@
+﻿program str_dkr2;
+var 
+  s:string;
+  l,bcount,scount,i:integer;
+  begin
+    write('Напечатайте текст: ');
+    readln(s);
+    
+    scount := 0;
+    bcount := 0;
+    l := length(s);
+    
+    for i := 1 to l do 
+    begin
+      if (s[i] in ['А'..'Я', 'а'..'я']) then
+        bcount := bcount +1
+    end;
+    
+    for i := 1 to l do 
+    begin
+      if s[i] = ' ' then 
+        scount := scount + 1
+    end;
+    
+    if l > 0 then 
+      scount := scount + 1;
+    
+    writeln('Ваш текст: ', s);
+    writeln('Количество букв в тексте: ', bcount);
+    writeln('Количество слов в тексте: ', scount);
+  end.
